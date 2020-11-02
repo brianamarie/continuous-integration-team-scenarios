3d3d6cfa4cadba96e53669a1a2a2a9d9eab5ca9d
Briana Swift <brianamarie@github.com>
2020-11-02 14:46:35 +0100

    add tests

--- ci.test.js
+++ ci.test.js
@@ -11,8 +11,19 @@ describe('CI sequence', () => {
     expect(/.*#.*/ig.test(fileContents)).toBe(true);
   });
 
-  // TODO add the tests between these comments =>
+  it('1. pull latest code', () => {
+    expect(/.*pull.*/ig.test(fileContents)).toBe(true);
+  });
 
-  // TODO <= add the tests between these comments
+  it('2. add commits', () => {
+    expect(/.*commit.*/ig.test(fileContents)).toBe(true);
+  });
 
+  it('3. push to the remote branch with the same name', () => {
+    expect(/.*push.*/ig.test(fileContents)).toBe(true);
+  });
+
+  it('4. open a pull request and continue working', () => {
+    expect(/.*pull\s+request.*/ig.test(fileContents)).toBe(true);
+  });
 });
