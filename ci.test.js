5806849f9a1ca1788b4e7efd8489b40722e188f0
Briana Swift <brianamarie@github.com>
2020-11-02 14:54:15 +0100

    add to ci.md

--- ci.test.js
+++ ci.test.js
@@ -26,4 +26,17 @@ describe('CI sequence', () => {
   it('4. open a pull request and continue working', () => {
     expect(/.*pull\s+request.*/ig.test(fileContents)).toBe(true);
   });
+
+  it('5. Merge/rebase commits from master. Make tests pass on the merge result.', () => {
+    expect(/.*merge.*commits.*tests\s+pass.*/ig.test(fileContents)).toBe(true);
+  });
+
+  it('6. Deploy from the feature branch to production.', () => {
+    expect(/.*Deploy.*to\s+production.*/ig.test(fileContents)).toBe(true);
+  });
+
+  it('7. If everything is good in production for some period of time, merge changes to master.', () => {
+    expect(/.*merge.*to\s+master.*/ig.test(fileContents)).toBe(true);
+  });
+  
 });
